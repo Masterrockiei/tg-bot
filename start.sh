15ab732e6693f1ca69c901d7bc9ad77cf358af69
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-08-15 18:46:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Masterrockiei/tg-bot.git /tg-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /tg-bot
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
